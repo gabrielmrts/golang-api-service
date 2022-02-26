1dad5325ee5a8c30845abbe85c2e6c41c1590231
Gabriel <gabrielmrtss@gmail.com>
2022-02-26 00:41:46 -0300

    Fix messages

--- routes/routes.go
+++ routes/routes.go
@@ -7,14 +7,9 @@ import (
 )
 
 func MakeRoutes(r *gin.Engine) {
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "a",
-		})
-	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"hello": "world",
+			"message": "working",
 		})
 	})
 }
