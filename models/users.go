3c590296602b861dee5fd3521bbc75d15f4d9edc
Gabriel <gabrielmrtss@gmail.com>
2022-02-27 00:12:08 -0300

    User methods

---
+++ models/users.go
@@ -0,0 +1,33 @@
+package models
+
+import "github.com/gabrielmrts/golang-api-service/factories"
+
+type User struct {
+	Username string
+	Email    string
+	Password string
+}
+
+func (s User) Create(user *User) int64 {
+	db := factories.GetDatabaseInstance()
+
+	stmt, err := db.Prepare("INSERT INTO users(username, email, password) VALUES($1, $2, $3)")
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := stmt.Exec(user.Username, user.Email, user.Password)
+
+	if err != nil {
+		panic(err)
+	}
+
+	affected, err := res.RowsAffected()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return affected
+}
